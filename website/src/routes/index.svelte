b40336f0a9385fb741bbb9779a1f95628b7654e3
legop3 <minecratfd8@gmail.com>
2022-03-22 18:29:50 -0400

    tabs workie

--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+      import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs';
+</script>
+
+<Tabs>
+    <TabList>
+        <Tab>one</Tab>
+        <Tab>two</Tab>
+        <Tab>three</Tab>
+    </TabList>
+
+
+    <TabPanel>
+        <h1>tab one</h1>
+    </TabPanel>
+
+
+    <TabPanel>
+        <h1>tab two</h1>
+    </TabPanel>
+
+    <TabPanel>
+        <h1>tab three</h1>
+    </TabPanel>
+
+</Tabs>
