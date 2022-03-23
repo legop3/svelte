f0c2aa3c321fb1b685f87dfc7f93224dff04dd68
legop3 <minecratfd8@gmail.com>
2022-03-23 14:14:11 -0400

    waht

--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,44 +1,57 @@
-<script>
+<script context="module">
     //   import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs';
-      import Tabs from './shared/Tabs.svelte'
+    //   import Tabs from './shared/Tabs.svelte'
       import Drawer from 'svelte-drawer-component';
-import Nested from 'src/pages/nested.svelte';
+// import About from 'src/pages/about.svelte';
+// import Nested from 'src/pages/nested.svelte';
       const modules = import.meta.glob("../pages/*.svelte")
     
     let tabitems = ["tab one", "tab two", "tab tljhf", "tabkfdsjhsf", "jkahf", "falkjshfkjsahfkh"]
     let activeitem = "tab one"
     let open = false;
+    let placement = "bottom";
+    let page
 
-    // console.log(modules)
-    for (const path in modules) {
-        modules[path]().then((mod) => {
-    console.log(path, mod)
-  })
-}
+    console.log(modules)
+//     for (const path in modules) {
+//         modules[path]().then((mod) => {
+//     console.log(path, mod)
+//     console.log(path)
+//     console.log(mod)
+//   })
+// }
+
+// for (let path in modules) {
+//     console.log(path)
+//     page = modules[path]
+//     console.log(page)
+// }
+
 
 
 </script>
 <style>
     .drawerbutton{
-        width: 100%;
-        height: 4em;
+        width: 10%;
+        height: 100%;
     }
 </style>
 
-
+<!-- <"../pages/nested"/> -->
 <main>
  
 </main>
 
-<Nested></Nested>
+
+<!-- <svelte:component this={"../pages/nested.svelte"}/> -->
+
 
 <button on:click={() => open = true}>MENU</button>
 
-<Drawer { open }  size='10em'>
+<Drawer { open } { placement }  size='10em'>
     <button on:click={() => open = false} class="drawerbutton">CLOSE</button>
     <!-- <Tabs {tabitems} {activeitem}></Tabs> -->
 </Drawer>
-
 
 
 
