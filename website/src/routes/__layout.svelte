0e4893603cab10857e562f9f9c02ce62513d0a04
legop3 <minecratfd8@gmail.com>
2022-03-24 00:51:38 -0400

    more styling

--- website/src/routes/__layout.svelte
+++ website/src/routes/__layout.svelte
@@ -1,3 +1,5 @@
+
+
 <script context="module">
     const modules = import.meta.glob("./*.svelte");
     const fileNames = Object.keys(modules).map(path => path.split('/').pop().split('.')[0]);      
@@ -11,38 +13,55 @@
     let open = false;
 </script>
 
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
 
 <slot />
 
-
+<!-- svelte-ignore a11y-missing-attribute -->
+<html>
 <button class="drawerbutton" on:click={() => open = true}>Open drawer</button>
 
-<div class="app">
-    <Drawer {open} size="20%" placement="bottom" on:clickAway={() => open = false}>
+<div class="drawer">
+    <Drawer {open} size="50%" placement="bottom" on:clickAway={() => open = false}>
         {#each fileNames as fileName}
             <button class="tablink" on:click={() => goto(`/${fileName!=='index'?fileName:''}`)}>{fileName}</button>
         {/each}
-        <button class="drawerbutton" on:click={() => open = false}>Close Drawer</button>
+        <!-- <button class="drawerbutton" on:click={() => open = false}>Close Drawer</button> -->
+        <!-- <p style="color: white; position:fixed; right:10%; z-index:10">test</p> -->
     </Drawer>
 </div>
-
+</html>
 
 <style>
+    
+    html {
+        display: flex;
+        padding: 0%;
+        margin: 0%;
+    }
+
     /* Style tab links */
     .tablink {
-        background-color: #555;
-        color: white;
-        float: right;
+        background-color: rgb(20, 54, 138);
+        color: rgb(0, 0, 0);
+        float:right;
         border: white;
-        outline: none;
+        align-self:stretch;
         cursor: pointer;
         padding: 14px 16px;
         font-size: 17px;
+        font-weight: bold;
         /* width: 10%; */
+
+        
     }
 
     .tablink:hover {
-        background-color: #777;
+        background-color: rgb(150, 72, 0);
     }
 
     .drawerbutton {
@@ -51,10 +70,14 @@
         bottom: 3%;
         background-color: cyan;
         padding: 2% 2%;
-        border: none;
+        border: 3px black;
+        /* border-radius: 10%; */
+        
     }
 
-    .app :global(.drawer .overlay) {
-        background: rgba(100, 100, 100, 0.5);
-    }
+  .drawer :global(.drawer .panel) {
+    background:rgba(67, 67, 67, 0.857);
+    /* border-radius: 10%; */
+}
+
 </style>
--- website/src/routes/nested.svelte
+++ website/src/routes/nested.svelte
@@ -1,11 +1,3 @@
 
 
-<script>
-    let w;
-    let h;
-</script>
-<h1>nestefkjdsajfaldskfjfdsafdsfd</h1>
-<img src="https://miro.medium.com/max/1400/1*Zjq72BWbHgOE3Feou7ABWA.jpeg" alt="" width="">
-<div bind:clientWidth={w} bind:clientHeight={h}>
-    afslkjdfhksadnyflkndsalfiundafaldsknfldsanyfliudsanflinydsalifnydslifnydsalifnyaldsifnylisadnyfliadsnyflayusndlfinyds
-</div>
+<img src="https://miro.medium.com/max/1400/1*Zjq72BWbHgOE3Feou7ABWA.jpeg" alt="" width="100%">
