97c8fa3e982a2252d624d9089d495424ab6926bb
legop3 <minecratfd8@gmail.com>
2022-03-25 16:18:42 -0400

    folder delete

--- website/src/routes/shared/home.svelte
+++ website/src/routes/shared/home.svelte
@@ -1,10 +1,13 @@
 <style>
     a {
     color: aqua;
-}
+    }
+    p {
+        background-color: darkslategray;
+    }
 </style>
 
-<h1>Home Page</h1>
+<h1>Landing Page</h1>
 <p>Welcome to Drawer. This is a place where I will be slowly adding tabs in the drawer, small web apps and stuff.</p>
 <p>Press the MENU button to see all of the tabs.</p>
-<p>Check out my <a href="https://github.com/legop3">GitHub</a></p>
+<p>Check out my <a href="https://github.com/legop3" target="_blank">GitHub</a></p>
