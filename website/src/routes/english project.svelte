2338cb5d4ec1ad6c008bd169aedecde460c6cbd9
legop3 <minecratfd8@gmail.com>
2022-03-25 13:31:33 -0400

    bug fix

--- website/src/routes/english project.svelte
+++ website/src/routes/english project.svelte
@@ -20,5 +20,5 @@
     <p>a remark, statement, or comment based on something one has seen, heard, or noticed.</p>
     <p style="text-indent: 1em; font-style: italic;">"he made a telling observation about Hugh"</p>
     <br><br>
-    <p>Observation is a good skill to have, it allows one to take in everything around themselves and apply this information to the problem they are solving. </p>
+    <p>Observation is a powerful skill. It allows you to take in your surroundings and decide things based on what you see. This skill is very useful in support situations, whether it's supporting people, supporting </p>
 </div>
