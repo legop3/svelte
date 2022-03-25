cf96b217ead5f230ca08e4ab7d9bb3a34e4e7a18
legop3 <minecratfd8@gmail.com>
2022-03-25 14:25:01 -0400

    project

--- website/src/routes/english project.svelte
+++ website/src/routes/english project.svelte
@@ -1,8 +1,8 @@
 
 <style>
     .english {
-        background-color: white;
-        color:rgb(33, 33, 33);
+        background-color: rgb(255, 255, 255);
+        color:rgb(0, 0, 0);
         max-width: 50em;
         padding: 10%;
         border-radius: 10px;
@@ -22,5 +22,6 @@
     <p style="text-indent: 1em; font-style: italic;">"he made a telling observation about Hugh"</p>
     <br><br>
     <p>Observation is a powerful skill. It allows you to take in your surroundings and decide things based on what you see. This skill is very useful in support situations, whether it's supporting people, supporting products, or supporting anything else, Observation is a great superpower to have.</p>
-    <p>In the past, Observation was used to keep track of history before we had means of measuring and archiving everything, and it still has much of the same purpose today.</p>
+    <p>In the past, observation was used to spread the truth about many issues, from an inside perspective of these issues observation was an extremley important aspect to spreading the facts around through writing to people who would have never known otherwise.</p>
+    <p>Today, neutral observation in controversial situations is still important to spreading raw facts around to people, whether they want to hear it or not, it is good to be educated about the world's situations even if you aren't directly involved.</p>
 </div>
