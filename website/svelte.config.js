21259a8f6b0d970297e8a46cd45824cf2f15185a
legop3 <minecratfd8@gmail.com>
2022-03-26 02:31:36 -0400

    chat broke

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -13,24 +13,6 @@ const config = {
 						const io = new Server(server.httpServer);
 
 
-
-						// This is located in the svelte config (see above "Socket.IO stuff goes here")
-							io.on('connection', (socket) => {
-								// Generate a random username and send it to the client to display it
-								let username = `User ${Math.round(Math.random() * 999999)}`;
-								socket.emit('name', username);
-
-								// Receive incoming messages and broadcast them
-								socket.on('message', (message) => {
-									io.emit('message', {
-										from: username,
-										message: message,
-										time: new Date().toLocaleString()
-									});
-								});
-							});
-
-
 						//socket.io stuff goes here
 
 						console.log('socketio injected');
