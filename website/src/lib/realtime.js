6abd49bb989f765da9207398359f360f882e2bc8
legop3 <minecratfd8@gmail.com>
2022-03-26 02:24:11 -0400

    port

--- website/src/lib/realtime.js
+++ website/src/lib/realtime.js
@@ -1,6 +1,6 @@
 // src/lib/realtime.js
 import ioClient from "socket.io-client";
-const ENDPOINT = "http://184.58.3.107:3000";
+const ENDPOINT = "http://184.58.3.107:1000";
 
 const socket = ioClient(ENDPOINT)
 
