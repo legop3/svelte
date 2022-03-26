3ad5b5d138c4e2b74de83eaa743f3deeb3809602
legop3 <minecratfd8@gmail.com>
2022-03-26 02:06:19 -0400

    chat and socketio added

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,10 +1,44 @@
 import adapter from '@sveltejs/adapter-auto';
-
+import {Server} from 'socket.io';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	//...
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		vite: {
+			plugins: [
+				{
+					name: 'sveltekit-socket-io',
+					configureServer(server) {
+						const io = new Server(server.httpServer);
+
+
+
+						// This is located in the svelte config (see above "Socket.IO stuff goes here")
+							io.on('connection', (socket) => {
+								// Generate a random username and send it to the client to display it
+								let username = `User ${Math.round(Math.random() * 999999)}`;
+								socket.emit('name', username);
+
+								// Receive incoming messages and broadcast them
+								socket.on('message', (message) => {
+									io.emit('message', {
+										from: username,
+										message: message,
+										time: new Date().toLocaleString()
+									});
+								});
+							});
+
+
+						//socket.io stuff goes here
+
+						console.log('socketio injected');
+					}
+				}
+			]
+		}
+	},
 };
 
 export default config;
